613bdb8a5890a1338fb49a02a47d0c42599b0258
stanimir003 <37873660+stanimir003@users.noreply.github.com>
2019-01-11 16:36:37 +0200

    Add files via upload

---
+++ Company/ComparatorByAge.java
@@ -0,0 +1,12 @@
+package Company.Comparators;
+
+import Company.Employee;
+
+import java.util.Comparator;
+
+public class ComparatorByAge implements Comparator<Employee> {
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return o1.getAge() - o2.getAge();
+    }
+}
